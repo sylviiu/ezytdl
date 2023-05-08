fee7f91195a9ca6948083971102692a0333cd948
Syl <hi@sylviiu.dev>
2023-05-07 19:13:27 -0500

    log err

--- util/errorHandler.js
+++ util/errorHandler.js
@@ -2,6 +2,7 @@ const superagent = require('superagent')
 
 module.exports = (err) => {
     if(`${err.toString().includes(`EADDRINUSE`)}`) {
+        console.log(`${err}`)
         superagent.get(`http://localhost:3000/focus`).then(() => {
             console.log(`Focused existing window, exiting...`);
             app.quit()
