4965e2943544a15a4e0be80491beb2287e68a97b
Syl <hi@sylviiu.dev>
2023-05-14 17:20:39 -0500

    remove unused scripts from html assets

--- html/assets/js/errorWS.js
+++
@@ -1,7 +0,0 @@
-const notificationWS = new WebSocket('ws://localhost:8080/download');
-
-notificationWS.onopen = () => {
-    notificationWS.send(`notification`)
-}
-
-notificationWS.onmessage = (e) => createNotification(JSON.parse(e.data))
--- html/assets/js/notificationWS.js
+++
@@ -1,4 +0,0 @@
-if(!document.getElementById(`loading`)) {
-    notifications.handler((content) => createNotification(content));
-    notifications.setReady();
-}
