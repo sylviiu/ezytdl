8b79386c39eb3968e7999ee19bf0679f311d00f5
Syl <hi@sylviiu.dev>
2023-05-15 10:37:57 -0500

    set tray to standard download icon, and invert colors if dark theme (testing)

--- core/tray.js
+++ core/tray.js
@@ -1,6 +1,10 @@
-const { app, Menu, Tray, nativeImage } = require('electron');
+const { app, Menu, Tray, nativeImage, nativeTheme } = require('electron');
 
 const { queue, queueEventEmitter, queueAction } = require(`../util/downloadManager`);
+
+const fs = require('fs');
+
+const buildTrayIcons = require(`../scripts/beforePack`);
 
 global.tray = null;
 
@@ -11,13 +15,52 @@ if(process.platform == `win32`) s = `\\`;
 
 const electronPath = require('electron').app.getAppPath();
 
-module.exports = () => {
-    const icon = (electronPath.includes(`app.asar`) ? `${electronPath.replace(`app.asar`, `app.asar.unpacked`)}/` : `./`) + (`res/packageIcons/icon-64x64.png`);
+module.exports = async () => {
+    const regularIcon = (electronPath.includes(`app.asar`) ? `${electronPath.replace(`app.asar`, `app.asar.unpacked`)}/` : __dirname.split(`core`).slice(0, -1).join(`core`)) + (`dist/trayIcons/circle-down-regular.png`);
+    const regularIconInv = (electronPath.includes(`app.asar`) ? `${electronPath.replace(`app.asar`, `app.asar.unpacked`)}/` : __dirname.split(`core`).slice(0, -1).join(`core`)) + (`dist/trayIcons/circle-down-regular-inv.png`);
+    const solidIcon = (electronPath.includes(`app.asar`) ? `${electronPath.replace(`app.asar`, `app.asar.unpacked`)}/` : __dirname.split(`core`).slice(0, -1).join(`core`)) + (`dist/trayIcons/circle-down-solid.png`);
+    const solidIconInv = (electronPath.includes(`app.asar`) ? `${electronPath.replace(`app.asar`, `app.asar.unpacked`)}/` : __dirname.split(`core`).slice(0, -1).join(`core`)) + (`dist/trayIcons/circle-down-solid-inv.png`);
 
-    global.tray = new Tray(icon);
+    console.log(regularIcon, solidIcon)
+
+    if(!fs.existsSync(regularIcon) || !fs.existsSync(solidIcon)) await buildTrayIcons();
+
+    global.tray = new Tray(regularIcon);
+
+    let current = `regular`;
+
+    const updateTray = (type) => {
+        console.log(`Updating tray -- type: ${type} / dark colors? ${nativeTheme.shouldUseDarkColors}`);
+
+        if(type == `regular`) {
+            if(nativeTheme.shouldUseDarkColors) {
+                global.tray.setImage(regularIcon);
+            } else {
+                global.tray.setImage(regularIconInv);
+            }
+            current = `regular`;
+        } else if(type == `solid`) {
+            if(nativeTheme.shouldUseDarkColors) {
+                global.tray.setImage(solidIcon);
+            } else {
+                global.tray.setImage(solidIconInv);
+            }
+            current = `solid`;
+        }
+    };
+
+    nativeTheme.on(`updated`, () => updateTray(current));
+
+    updateTray(`regular`);
 
     const createArrayAndApply = (queue) => {
         const length = Object.values(queue).slice(1).reduce((a,b) => a+b.length, 0);
+
+        if((length + queue.complete.length) == 0 && current == `solid`) {
+            updateTray(`regular`)
+        } else if((length + queue.complete.length) > 0 && current == `regular`) {
+            updateTray(`solid`)
+        }
 
         const a = [];
 
---
+++ scripts/beforePack.js
@@ -0,0 +1,61 @@
+const { convert, convertFile } = require('convert-svg-to-png');
+const jimp = require(`jimp`)
+
+const fs = require('fs');
+
+const outputFilePath = (type, mult) => `dist/trayIcons/circle-down-${type}${mult == 1 ? `` : `@` + mult + `x`}.png`;
+
+const createTrayImage = (name, opt, mult) => new Promise(async res => {
+    const regularPath = outputFilePath(`${name}`, mult);
+    const invertedPath = outputFilePath(`${name}-inv`, mult);
+
+    console.log(`getting buffer of ${name} @ ${opt.width}`)
+
+    const buf = fs.readFileSync('res/trayIcons/' + `circle-down-` + name + '.svg');
+    const png = await convert(buf, opt);
+    
+    fs.writeFileSync(regularPath, png);
+
+    console.log(`getting inverted buffer of ${name} @ ${opt.width}`)
+
+    const inv = await jimp.read(png);
+    inv.invert();
+    inv.getBuffer(jimp.MIME_PNG, (err, invBuf) => {
+        if(!err) {
+            fs.writeFileSync(invertedPath, invBuf);
+            res();
+        } else throw new Error(`Failed to invert ${name} @ ${opt.width}`, err)
+    });
+})
+
+module.exports = (context) => new Promise(async res => {
+    let sizes = [ 16 ];
+
+    let supportedMultipliers = [ 1, 1.25, 1.33, 1.4, 1.5, 1.8, 2, 2.5, 3, 4, 5 ]
+    //https://github.com/electron/electron/blob/main/docs/api/native-image.md#template-image
+
+    for(mult of supportedMultipliers.slice(1)) {
+        sizes.push( 16 * mult );
+    }
+
+    fs.mkdirSync(`dist/trayIcons`, { recursive: true });
+
+    for(let i in sizes) {
+        const size = sizes[i];
+
+        const opt = {
+            width: size,
+            height: size,
+        }
+
+        if(!fs.existsSync(outputFilePath(`regular`))) {
+            await createTrayImage(`regular`, opt, supportedMultipliers[i]);
+        } else console.log(`Skipping ${outputFilePath(`regular`)} (already exists)`)
+
+        if(!fs.existsSync(outputFilePath(`solid`))) {
+            await createTrayImage(`solid`, opt, supportedMultipliers[i]);
+        } else console.log(`Skipping ${outputFilePath(`solid`)} (already exists)`)
+    };
+
+    res(true);
+});
