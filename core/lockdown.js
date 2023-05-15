62dffa36745edbe632ecff0b26ed30219ca9d9b2
Syl <hi@sylviiu.dev>
2023-05-14 21:39:47 -0500

    add url open handler to lockdown
    urls will NOT open in the window as per security standards of electron -- if it is recognized as a web url, it will only open it in an external browser on the system.

--- core/lockdown.js
+++ core/lockdown.js
@@ -1,11 +1,26 @@
-const { BrowserWindow, app, globalShortcut, session } = require('electron');
+const { app, globalShortcut, session, shell } = require('electron');
 
 module.exports = () => {
+    const genericURLRegex = /^https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)$/i;
+
+    const urlHandler = (e, url) => {
+        console.log(`urlHandler:`, url)
+
+        if(`${url}`.match(genericURLRegex)) {
+            console.log(`opening in new window (url regex matched)`)
+            e.preventDefault();
+            shell.openExternal(url);
+        } else {
+            console.log(`opening in app`)
+        }
+    }
+
+    global.window.webContents.on('new-window', urlHandler);
+    global.window.webContents.on('will-navigate', urlHandler);
+
     app.on('web-contents-created', (event, contents) => {
         contents.setWindowOpenHandler(({ url }) => {
-            const genericURLRegex = /^https?:\/\/(www\.)?[^\/]+\/?$/i
-
-            if (`${url}`.match(genericURLRegex)) shell.openExternal(url)
+            if(`${url}`.match(genericURLRegex)) shell.openExternal(url)
         
             return { action: 'deny' }
         })
