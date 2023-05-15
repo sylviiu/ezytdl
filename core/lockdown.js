eccdf49dcdc0d413035a99e0855edd36a4eb56b3
Syl <hi@sylviiu.dev>
2023-05-14 22:31:16 -0500

    prevent opening new windows (they always end up broken)

--- core/lockdown.js
+++ core/lockdown.js
@@ -1,5 +1,7 @@
 const { app, globalShortcut, session, shell } = require('electron');
 
+const path = require('path');
+
 module.exports = () => {
     const genericURLRegex = /^https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)$/i;
 
@@ -8,15 +10,28 @@ module.exports = () => {
 
         if(`${url}`.match(genericURLRegex)) {
             console.log(`opening in new window (url regex matched)`)
-            e.preventDefault();
+            if(e && e.preventDefault) e.preventDefault();
             shell.openExternal(url);
         } else {
-            console.log(`opening in app`)
+            console.log(`opening in app`);
         }
     }
 
-    global.window.webContents.on('new-window', urlHandler);
     global.window.webContents.on('will-navigate', urlHandler);
+
+    global.window.webContents.setWindowOpenHandler((e) => {
+        console.log(`prevented new window;`, e.url)
+        if(e.url && e.url.startsWith(`file:///`)) {
+            global.window.loadFile(path.join(__dirname.split(`core`).slice(0, -1).join(`core`), `html`, e.url.split(`/`).slice(-1)[0]));
+        } else if(e.url) urlHandler(null, e.url)
+        return { action: `deny` }
+    });
+
+    global.window.webContents.on(`new-window`, (e, url) => {
+        e.stopImmediatePropagation();
+        e.preventDefault();
+        global.window.loadFile(url);
+    }, true)
 
     app.on('web-contents-created', (event, contents) => {
         contents.setWindowOpenHandler(({ url }) => {
