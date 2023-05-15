89a8c0fa7c9a6b26dc9c1013483e5f671ec8ed4c
Syl <hi@sylviiu.dev>
2023-05-15 18:47:35 -0500

    make titlebar buttons look like buttons and not links

--- html/topjs/feelLikeNativeApp.js
+++ html/topjs/feelLikeNativeApp.js
@@ -3,22 +3,53 @@
 addEventListener(`DOMContentLoaded`, () => {
     document.body.style.userSelect = `none`;
 
-    if(navigator.platform == `Win32` && document.getElementById(`windowControls`)) {
-        const winControls = document.getElementById(`windowControls`);
-        winControls.classList.remove(`d-none`);
-        winControls.classList.add(`d-flex`);
-
-        document.getElementById(`minimizeWindows`).onclick = () => windowControls.minimize();
-        document.getElementById(`maximizeWindows`).onclick = () => windowControls.maximize();
-        document.getElementById(`closeWindows`).onclick = () => windowControls.close();
-
-        document.getElementById(`navigationBar`).style[`-webkit-app-region`] = `drag`;
-
-        const disableDrag = (node) => {
-            if(node.childNodes) node.childNodes.forEach(n => disableDrag(n));
-            if(node && node.classList && node.classList.contains(`btn`)) node.style[`-webkit-app-region`] = `no-drag`;
+    if(document.getElementById(`navigationBar`)) {
+        document.querySelectorAll(`#navigationBar .btn`).forEach(btn => {
+            btn.style.cursor = `default`;
+            const children = [];
+            btn.childNodes.forEach(n => { if(n && n.style) children.push(n) })
+            btn.onmouseover = () => {
+                anime.remove(children)
+                anime({
+                    targets: children,
+                    scale: 1.4,
+                    duration: 500,
+                    easing: `easeOutExpo`
+                })
+            };
+            btn.onmouseout = () => {
+                anime.remove(children)
+                anime({
+                    targets: children,
+                    scale: 1,
+                    duration: 500,
+                    easing: `easeOutExpo`
+                })
+            }
+        });
+        
+        if(document.getElementById(`windowControls`)) {
+            console.log(`windowControls present`);
+            if(navigator.platform == `Win32` || navigator.platform.indexOf(`Mac`) === 0) {
+                const winControls = document.getElementById(`windowControls`);
+                winControls.classList.remove(`d-none`);
+                winControls.classList.add(`d-flex`);
+        
+                document.getElementById(`minimizeWindows`).onclick = () => windowControls.minimize();
+                document.getElementById(`maximizeWindows`).onclick = () => windowControls.maximize();
+                document.getElementById(`closeWindows`).onclick = () => windowControls.close();
+        
+                document.getElementById(`navigationBar`).style[`-webkit-app-region`] = `drag`;
+        
+                const disableDrag = (node) => {
+                    if(node.childNodes) node.childNodes.forEach(n => disableDrag(n));
+                    if(node && node.classList && node.classList.contains(`btn`)) node.style[`-webkit-app-region`] = `no-drag`;
+                }
+        
+                document.getElementById(`navigationBar`).childNodes.forEach(element => disableDrag(element))
+            } else {
+                document.getElementById(`windowControls`).classList.add(`d-none`)
+            }
         }
-
-        document.getElementById(`navigationBar`).childNodes.forEach(element => disableDrag(element))
-    }
+    } else if(navigator.platform == `Win32` || navigator.platform.indexOf(`Mac`) === 0) document.body.style[`-webkit-app-region`] = `drag`;
 });
