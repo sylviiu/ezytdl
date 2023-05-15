9905b4a69d43a8d0b9782de6f7531f5372b12cd0
Syl <hi@sylviiu.dev>
2023-05-14 21:41:00 -0500

    if the window has not been created or ezytdl is minimized to tray, and a notification that is allowed to send to system is sent, it will be sent through the system's notifications instead

--- core/sendNotification.js
+++ core/sendNotification.js
@@ -1,9 +1,15 @@
+const { Notification } = require('electron');
+
 const notifQueue = [];
 
 module.exports = (content) => {
     console.log(`sending notification ${global.window ? true : false}`, content);
 
-    if(!global.window) {
+    if((!global.window || global.windowHidden) && content.systemAllowed) {
+        const notif = new Notification({ title: (content.type ? `[${content.type.toUpperCase()}] ` : ``) + content.headingText, body: content.bodyText });
+        notif.show();
+        return true;
+    } else if(!global.window || global.windowHidden) {
         notifQueue.push(content);
         return false;
     } else {
