61e185b7d32f1795094c071af524a9d8999f5523
Syl <hi@sylviiu.dev>
2023-05-18 18:19:45 -0500

    fix app

--- index.js
+++ index.js
@@ -3,14 +3,15 @@ const startTime = Date.now();
 global.testrun = process.argv.find(s => s == `--testrun`) ? true : false;
 global.headless = process.argv.find(s => s == `--headless`) ? true : false;
 
+console.log(`Starting ezytdl v${require(`./package.json`).version}`)
+
+const { app, ipcMain } = require(`electron`);
+
 if(global.testrun || global.headless) {
     app.commandLine.appendSwitch(`--ignore-gpu-blacklist`);
     app.disableHardwareAcceleration();
 }
 
-console.log(`Starting ezytdl v${require(`./package.json`).version}`)
-
-const { app, ipcMain } = require(`electron`);
 global.configPath = app.getPath('userData');
 
 process.on(`uncaughtException`, (err) => (global.testrun ? require(`./util/errorAndExit`) : require(`./util/errorHandler`))(`${typeof err == `object` ? JSON.stringify(err, null, 4) : err}\n\n${err.stack? err.stack : `(no stack)`}`))
