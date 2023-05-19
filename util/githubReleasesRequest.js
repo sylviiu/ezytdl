2b5feb55b78bccda7be64b4c954956da7dfc78d2
Syl <hi@sylviiu.dev>
2023-05-19 10:58:33 -0500

    authorize w GITHUB_TOKEN on testruns

--- util/githubReleasesRequest.js
+++ util/githubReleasesRequest.js
@@ -1,6 +1,11 @@
 module.exports = (creator, repo) => new Promise(async (res, rej) => {
     const ghRequest = require(`superagent`).get(`https://api.github.com/repos/${creator}/${repo}/releases?page=1&per_page=1`).set(`User-Agent`, `node`);
 
+    if(process.env["GITHUB_TOKEN"] && process.testrun) {
+        console.log(`[TESTRUN] GITHUB_TOKEN found in environment! Authorizing this release request`)
+        ghRequest.set(`Authorization`, process.env["GITHUB_TOKEN"])
+    }
+
     ghRequest.then(r => r.body).then(r => {
         return res({
             version: r[0].tag_name,
