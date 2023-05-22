5597fe3347f792288be6e7e215ed47f86f673199
Syl <hi@sylviiu.dev>
2023-05-22 14:58:03 -0500

    create nightly release notes generator

---
+++ devscripts/generateReleaseNotes.js
@@ -0,0 +1,35 @@
+// designated for linux
+
+if(process.argv.find(s => s == `nightly`)) {
+    const fs = require(`fs`);
+    const child_process = require(`child_process`);
+    
+    const commits = child_process.execSync(`git log $(git rev-list -n 1 $(git describe --tags --abbrev=0))...$(git rev-parse HEAD)`, {shell: `/bin/bash`}).toString().trim();
+    
+    const daysOfTheWeek = [`Sun`, `Mon`, `Tue`, `Wed`, `Thu`, `Fri`, `Sat`];
+    
+    const months = [`Jan`, `Feb`, `Mar`, `Apr`, `May`, `Jun`, `Jul`, `Aug`, `Sep`, `Oct`, `Nov`, `Dec`]
+    
+    const str = commits.slice(7).split(`\n\ncommit `).map(s => {
+        const hash = s.split(`\n`)[0];
+        const hashLink = `[**${hash.slice(0, 7)}**](https://github.com/sylviiu/ezytdl/commit/${hash})`;
+        //console.log(hashLink);
+    
+        const author = s.split(`\n`)[1].trim().split(`: `).slice(1).join(`:`);
+    
+        const date = new Date(s.split(`\n`)[2].split(`:`).slice(1).join(`:`).trim())
+        const hour = date.getUTCHours().toString().length == 1 ? `0${date.getUTCHours()}` : date.getUTCHours()
+        const min = date.getUTCMinutes().toString().length == 1 ? `0${date.getUTCMinutes()}` : date.getUTCMinutes()
+        const sec = date.getUTCSeconds().toString().length == 1 ? `0${date.getUTCSeconds()}` : date.getUTCSeconds()
+        const dateStr = `${daysOfTheWeek[date.getUTCDay()]}, ${date.getUTCDate()} ${months[date.getUTCMonth()]}, ${date.getUTCFullYear()} @ ${hour}:${min}:${sec} UTC`
+        //console.log(dateStr)
+    
+        const parsed = `- ${author}: ${hashLink} / ${dateStr}\n> ${s.split(`\n`).slice(4).map(s => s.trim()).join(`\n> `)}\n`;
+    
+        return parsed;
+    });
+    
+    console.log(str);
+    
+    fs.writeFileSync(`./release-notes.md`, fs.readFileSync(`./res/releaseNotes/nightly.md`).toString() + str.join(`\n`))
+}
---
+++ res/releaseNotes/nightly.md
@@ -0,0 +1,13 @@
+<h1 align="center">
+    <img src="https://raw.githubusercontent.com/sylviiu/ezytdl/0d3b465f74edb3c94887fe137ac66826fc1bfc4f/buildResources/packageIcons/icon-256x256.png" width="64px"/><br>
+    This is a nightly build.
+</h1>
+
+## These builds are not guaranteed to be functional. These are designated builds to test new features before they become an official release!
+
+### If you are looking for a stable build, [please download the latest release.](https://github.com/sylviiu/ezytdl/releases/latest)
+
+--------------
+
+### Changelog
+
