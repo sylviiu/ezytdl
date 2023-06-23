4d92d2d7c63c8ce74db9b4ca9a1a642d177a8c3b
Syl <hi@sylviiu.dev>
2023-06-23 03:06:29 -0500

    separate bridge from app bundle -- this can be updated separately now :D

--- checks/ytdlpIsDownloaded.js
+++ checks/ytdlpIsDownloaded.js
@@ -1,11 +1,27 @@
 module.exports = (doNotCheckForUpdates) => new Promise(async res => {
-    const { file, getPath } = require(`../util/filenames/ytdlp`);
-    const fs = require(`fs`);
+    const { getPath } = require(`../util/filenames/pybridge`);
 
-    const bridgepath = require(`../util/pythonBridge`).bridgepath;
+    const path = getPath();
 
-    if(/*fs.existsSync(bridgepath)*/ true) {
+    const spawnBridge = async () => {
         console.log(`BRIDGE PATH EXISTS.`);
+
+        if(!doNotCheckForUpdates) await new Promise(async r => {
+            latestVersion().then(async o => {
+                console.log(`Latest version available is ${o.version}`);
+
+                const versionDownloaded = await require(`../util/currentVersion/ytdlp`)(true);
+
+                console.log(`Version downloaded is ${versionDownloaded}`);
+
+                if(versionDownloaded == o.version) {
+                    return r();
+                } else {
+                    return res(false)
+                }
+            });
+        })
+
         if(!global.createdBridge) {
             console.log(`creating bridge`)
             require(`../util/pythonBridge`).create().then(() => {
@@ -13,33 +29,9 @@ module.exports = (doNotCheckForUpdates) => new Promise(async res => {
                 return res(true)
             })
         } else return res(true)
-    } else {
-        console.log(`falling back to binary distribution`)
-        console.log(`Looking for file ${file}`);
-    
-        if(getPath()) {
-            if(doNotCheckForUpdates) return res(true);
-    
-            const latestVersion = require(`../util/fetchLatestVersion/ytdlp`);
-        
-            latestVersion().then(async o => {
-                console.log(`Latest version available is ${o.version}`);
-    
-                const versionDownloaded = await require(`../util/currentVersion/ytdlp`)(true);
-    
-                console.log(`Version downloaded is ${versionDownloaded}`);
-    
-                if(versionDownloaded == o.version) {
-                    return res(true)
-                } else {
-                    return res(false)
-                }
-            }).catch(e => {
-                // It's already downloaded and usable, so if the latest version is unavailable, we can still use it.
-                return res(true)
-            })
-        } else {
-            return res(false)
-        }
     }
+
+    if(path) {
+        spawnBridge();
+    } else res(false);
 })
