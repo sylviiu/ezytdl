af80b0e4962adc6358a3a22ef4bf349906aee5a1
Syl <hi@sylviiu.dev>
2023-06-29 20:38:22 -0500

    fixed "installed" strings in settings page

--- core/ipc/update/getVersion.js
+++ core/ipc/update/getVersion.js
@@ -1,7 +1,9 @@
+const getPath = require('../../../util/getPath');
+
 module.exports = {
     type: `handle`,
     func: (_e, name) => {
-        if(require('fs').existsSync(`./util/currentVersion/${name}.js`)) {
+        if(require('fs').existsSync(getPath(`./util/currentVersion/${name}.js`, true))) {
             return require(`../../../util/currentVersion/${name}`)(false, true)
         } else return null;
     }
