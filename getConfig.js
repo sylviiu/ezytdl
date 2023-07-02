7c5dcfd4da60eba1dd5361bf53ffef5e2a824733
Syl <hi@sylviiu.dev>
2023-07-02 10:56:34 -0500

    custom config object parsing

--- getConfig.js
+++ getConfig.js
@@ -9,37 +9,51 @@ let newSettingsNotifSent = false;
 
 let firstCheckDone = false;
 
-module.exports = (configObject) => {
+module.exports = (configObject, {
+    source=`./defaultConfig.json`,
+    target=`config.json`,
+    allowNonexistentRemoval=true,
+    allowChangedDefaults=true,
+}={}) => {
+    const custom = source != `./defaultConfig.json` && target != `config.json` ? true : false;
+
     try {
-        const defaultConfig = Object.assign({}, require(`./defaultConfig.json`));
+        const defaultConfig = Object.assign({}, require(source));
 
-        const gpuArgs = require(`./util/ffmpegGPUArgs.json`);
+        if(!custom) {
+            // add ffmpeg hardware acceleration toggles to config
+            const gpuArgs = require(`./util/ffmpegGPUArgs.json`);
+            for(const key of Object.keys(gpuArgs)) {
+                if(gpuArgs[key].platform.includes(process.platform)) {
+                    defaultConfig.hardwareAcceleratedConversion[key] = false;
+                }
+            };
 
-        // add ffmpeg hardware acceleration options to config
-        for(const key of Object.keys(gpuArgs)) {
-            if(gpuArgs[key].platform.includes(process.platform)) {
-                defaultConfig.hardwareAcceleratedConversion[key] = false;
-            }
+            // add ffmpeg conversion preset toggles to config
+            const ffmpegPresets = require(`./util/ffmpegPresets.json`);
+            for(const {key, defaultEnabled} of ffmpegPresets) {
+                defaultConfig.ffmpegPresets[key] = defaultEnabled || false;
+            };
         }
     
         fs.mkdirSync(global.configPath, { recursive: true, failIfExists: false });
         
         let checked = false;
 
-        if(!fs.existsSync(`${global.configPath}/config.json`)) {
-            fs.writeFileSync(`${global.configPath}/config.json`, JSON.stringify(defaultConfig, null, 4), { encoding: `utf-8` });
+        if(!fs.existsSync(`${global.configPath}/${target}`)) {
+            fs.writeFileSync(`${global.configPath}/${target}`, JSON.stringify(defaultConfig, null, 4), { encoding: `utf-8` });
             checked = true;
         } else {
             try {
-                JSON.parse(fs.readFileSync(`${global.configPath}/config.json`));
+                JSON.parse(fs.readFileSync(`${global.configPath}/${target}`));
             } catch(e) {
-                fs.unlinkSync(`${global.configPath}/config.json`);
+                fs.unlinkSync(`${global.configPath}/${target}`);
                 module.exports(configObject);
             }
         };
 
         const checkKeys = (logPrefix, thisKey, config, defaults, addDefaults, removeNonexistent) => {
-            if(removeNonexistent) {
+            if(removeNonexistent && allowNonexistentRemoval) {
                 for(const key of Object.keys(config)) {
                     if(typeof defaults[key] == 'undefined') {
                         delete config[key];
@@ -50,7 +64,7 @@ module.exports = (configObject) => {
 
             for(const key of Object.keys(defaults)) {
                 if(addDefaults && typeof config[key] == `undefined`) {
-                    if(!newSettingsNotifSent && !firstCheckDone) {
+                    if(!newSettingsNotifSent && !firstCheckDone && !custom) {
                         newSettingsNotifSent = true;
                         sendNotification({
                             headingText: `New settings!`,
@@ -67,7 +81,10 @@ module.exports = (configObject) => {
                 } else {
                     //console.log(`key`, key, `type`, typeof config[key], `value`, config[key], `default type`, typeof defaults[key], `default value`, defaults[key])
                     
-                    if(defaults[key] && typeof defaults[key] == `object`) {
+                    if(!allowChangedDefaults && defaults[key] && defaults[key] != config[key]) {
+                        config[key] = defaults[key];
+                        checked = true;
+                    } else if(defaults[key] && typeof defaults[key] == `object`) {
                         //console.log(`checking keys for ${key}`)
                         config[key] = checkKeys(logPrefix + ` > `, thisKey + ` / ` + key, config[key], defaults[key], addDefaults, removeNonexistent);
                     } else {
@@ -82,7 +99,7 @@ module.exports = (configObject) => {
                         } else if(typeof config[key] != typeof defaults[key]) {
                             sendNotification({
                                 type: `warn`,
-                                headingText: `Config key mismatch!`,
+                                headingText: `Config key mismatch! (${target}: ${key})`,
                                 bodyText: `The config key "${key}" is missing or is of the wrong type! (Expected: ${typeof defaults[key]}, got: ${config[key] ? typeof config[key] : ``})`
                             });
                             //console.log(config[key], defaults[key])
@@ -97,7 +114,7 @@ module.exports = (configObject) => {
         }
         
         if(!checked) {
-            const config = JSON.parse(fs.readFileSync(`${global.configPath}/config.json`));
+            const config = JSON.parse(fs.readFileSync(`${global.configPath}/${target}`));
 
             const checkedConfig = checkKeys(`> `, `root config object`, config, defaultConfig, true, true);
 
@@ -105,77 +122,65 @@ module.exports = (configObject) => {
             
             if(checked) {
                 console.log(`Updated config!`)
-                fs.writeFileSync(`${global.configPath}/config.json`, JSON.stringify(checkedConfig, null, 4), { encoding: `utf-8` });
+                fs.writeFileSync(`${global.configPath}/${target}`, JSON.stringify(checkedConfig, null, 4), { encoding: `utf-8` });
             }
         };
         
         if(configObject) {
-            const config = JSON.parse(fs.readFileSync(`${global.configPath}/config.json`));
+            const config = JSON.parse(fs.readFileSync(`${global.configPath}/${target}`));
 
             const checkedConfig = checkKeys(`> `, `updated config object`, configObject, defaultConfig);
             
-            fs.writeFileSync(`${global.configPath}/config.json`, JSON.stringify(Object.assign({}, config, checkedConfig), null, 4), { encoding: `utf-8` });
+            fs.writeFileSync(`${global.configPath}/${target}`, JSON.stringify(Object.assign({}, config, checkedConfig), null, 4), { encoding: `utf-8` });
 
-            if(configObject.alwaysUseLightIcon != undefined) require(`./core/downloadIcon.js`).set();
+            if(!custom && configObject.alwaysUseLightIcon != undefined) require(`./core/downloadIcon.js`).set();
         };
 
-        const userConfig = JSON.parse(fs.readFileSync(`${global.configPath}/config.json`))
+        const userConfig = JSON.parse(fs.readFileSync(`${global.configPath}/${target}`));
 
-        let slashUsed = require('os').platform() == `win32` ? `\\` : `/`
-
-        if(!userConfig.saveLocation) userConfig.saveLocation = require(`path`).join((fs.existsSync(require(`path`).join(os.homedir(), `Downloads`)) ? require(`path`).join(os.homedir(), `Downloads`) : os.homedir()), `ezytdl`);
-
-        if(userConfig.downloadFromClipboard) {
-            global.downloadFromClipboard = true;
-        } else {
-            global.downloadFromClipboard = false;
-        }
-
-        firstCheckDone = true;
-
-        userConfig.strings = Object.assign(require(`./configStrings.json`), {
-            hardwareAcceleratedConversionExtended: {}
-        });
-
-        // add ffmpeg hardware acceleration options to config strings
-        for(const key of Object.keys(gpuArgs)) {
-            if(gpuArgs[key].platform.includes(process.platform)) {
-                userConfig.strings.hardwareAcceleratedConversionExtended[key] = gpuArgs[key].name;
-            }
-        }
-
-        userConfig.descriptions = Object.assign(require(`./configDescriptions.json`), {
-            hardwareAcceleratedConversionExtended: {}
-        });
-
-        // add ffmpeg hardware acceleration options to config descriptions
-        for(const key of Object.keys(gpuArgs)) {
-            if(gpuArgs[key].platform.includes(process.platform)) {
-                userConfig.descriptions.hardwareAcceleratedConversionExtended[key] = gpuArgs[key].description;
-            }
-        }
-
-        userConfig.actions = {};
-
-        const parseAction = (key, obj, actionsObj) => {
-            if(obj.func) {
-                actionsObj[key] = {
-                    name: obj.name || `ooh fancy button :D`,
-                    args: obj.args || [],
-                    manuallySavable: typeof obj.manuallySavable == `boolean` ? obj.manuallySavable : true,
-                    confirmation: obj.confirmation || null,
-                };
+        if(!custom) {
+            if(!userConfig.saveLocation) userConfig.saveLocation = require(`path`).join((fs.existsSync(require(`path`).join(os.homedir(), `Downloads`)) ? require(`path`).join(os.homedir(), `Downloads`) : os.homedir()), `ezytdl`);
+    
+            if(userConfig.downloadFromClipboard) {
+                global.downloadFromClipboard = true;
             } else {
-                if(key) {
-                    actionsObj[key + `Extended`] = {}
-                    for(const entry of Object.entries(obj)) parseAction(entry[0], entry[1], actionsObj[key + `Extended`]);
-                } else {
-                    for(const entry of Object.entries(obj)) parseAction(entry[0], entry[1], actionsObj);
-                }
+                global.downloadFromClipboard = false;
             }
-        };
+    
+            if(!custom) firstCheckDone = true;
+    
+            userConfig.strings = require(`./configStrings.json`)
+            userConfig.descriptions = require(`./configDescriptions.json`);
+    
+            for(const extension of Object.entries(require(`./core/userConfExtensions.js`))) {
+                console.log(`adding userconf extension ${extension[0]}`);
+                extension[1](userConfig);
+            }
+    
+            userConfig.actions = {};
+    
+            const parseAction = (key, obj, actionsObj) => {
+                if(obj.func) {
+                    actionsObj[key] = {
+                        name: obj.name || `ooh fancy button :D`,
+                        args: obj.args || [],
+                        manuallySavable: typeof obj.manuallySavable == `boolean` ? obj.manuallySavable : true,
+                        confirmation: obj.confirmation || null,
+                    };
+                } else {
+                    if(key) {
+                        actionsObj[key + `Extended`] = {}
+                        for(const entry of Object.entries(obj)) parseAction(entry[0], entry[1], actionsObj[key + `Extended`]);
+                    } else {
+                        for(const entry of Object.entries(obj)) parseAction(entry[0], entry[1], actionsObj);
+                    }
+                }
+            };
+    
+            parseAction(null, require(`./core/configActions.js`)(userConfig), userConfig.actions);
 
-        parseAction(null, require(`./core/configActions.js`)(userConfig), userConfig.actions);
+            console.log(userConfig)
+        }
 
         return userConfig;
     } catch(e) {
