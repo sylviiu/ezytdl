51132a055ba94273bc7b99f77324046ba35f9c84
Syl <hi@sylviiu.dev>
2023-07-05 07:05:10 -0500

    color modifications :3

--- util/parseSystemColors.js
+++ util/parseSystemColors.js
@@ -4,30 +4,50 @@ const range = (val) => {
     if (val < 0) return 0;
     if (val > 255) return 255;
     return val;
-}
+};
+
+const tinycolor = require('tinycolor2');
+const ColorScheme = require('color-scheme');
 
 module.exports = ({ r, g, b }) => {
-    const lowest = Math.min(r, g, b);
-    return {
+    const scm = new ColorScheme;
+
+    const hex = tinycolor(`#${r.toString(16).toUpperCase()}${g.toString(16).toUpperCase()}${b.toString(16).toUpperCase()}`).saturate(50).toHexString().replace(`#`, ``)
+
+    const colors = scm.from_hex(hex)
+    .scheme('analogic')
+    .distance(0.9)
+    .add_complement(false)
+    .variation('pastel')
+    .web_safe(false)
+    .colors();
+
+    const standard = colors[0];
+    const light = tinycolor(`#` + colors[4]).saturate(25).brighten(25).toHexString().replace(`#`, ``);
+    const dark = colors[1];
+
+    const newColors = {
         standard: {
-            r: range(r),
-            g: range(g),
-            b: range(b),
+            r: range(parseInt(standard.slice(0, 2), 16)),
+            g: range(parseInt(standard.slice(2, 4), 16)),
+            b: range(parseInt(standard.slice(4, 6), 16)),
         },
         light: {
-            r: range(r + ((255-lowest)/2)),
-            g: range(g + ((255-lowest)/2)),
-            b: range(b + ((255-lowest)/2)),
+            r: range(parseInt(light.slice(0, 2), 16)),
+            g: range(parseInt(light.slice(2, 4), 16)),
+            b: range(parseInt(light.slice(4, 6), 16)),
         },
         dark: {
-            r: range(r/7.5),
-            g: range(g/7.5),
-            b: range(b/7.5),
+            r: range(parseInt(dark.slice(0, 2), 16)),
+            g: range(parseInt(dark.slice(2, 4), 16)),
+            b: range(parseInt(dark.slice(4, 6), 16)),
         },
         darker: {
-            r: range(r/10),
-            g: range(g/10),
-            b: range(b/10),
+            r: range(parseInt(dark.slice(0, 2), 16)/10),
+            g: range(parseInt(dark.slice(2, 4), 16)/10),
+            b: range(parseInt(dark.slice(4, 6), 16)/10),
         },
-    }
+    };
+
+    return newColors
 }
