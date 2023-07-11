241f08e4b202f9bf07433949d335c57714e3791e
Syl <hi@sylviiu.dev>
2023-07-10 21:11:59 -0500

    fix "creating config" log

--- init/configs.js
+++ init/configs.js
@@ -2,7 +2,7 @@ module.exports = () => new Promise(async res => {
     const configs = require(`../util/configs`);
 
     for(const conf of Object.entries(configs)) {
-        console.log(`Creating config ${conf[0]}... (typeof ${conf[1]})`)
+        console.log(`Creating config ${conf[0]}... (typeof ${typeof conf[1]})`)
         await conf[1]();
     };
 
