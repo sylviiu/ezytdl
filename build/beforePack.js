a8307f9b31db0a9becb2fed4e64d50fc7ce0b541
Syl <hi@sylviiu.dev>
2023-07-12 18:25:15 -0500

    minify object

--- build/beforePack.js
+++ build/beforePack.js
@@ -7,14 +7,14 @@ module.exports = (context) => {
     console.log(`minifying ${dirs.map(o => o.files.length).reduce((a,b) => a + b, 0)} files in ${dirs.length} dirs... (${dirs.map(o => `${o.path} with ${o.files.length} files`).join(`, `)})`);
 
     dirs.forEach(dir => {
-        const overallScript = [];
+        const overallScript = {};
 
         dir.files.forEach(file => {
-            overallScript.push(fs.readFileSync(`${dir.path}/${file}`, 'utf8'));
+            overallScript[file] = fs.readFileSync(`${dir.path}/${file}`, 'utf8');
             fs.rmSync(`${dir.path}/${file}`);
         });
 
-        const minified = uglify.minify(overallScript.join(`\n`), { compress: { drop_console: true } }).code;
+        const minified = uglify.minify(overallScript, { compress: { drop_console: true } }).code;
 
         fs.writeFileSync(`${dir.path}/minified.js`, minified, 'utf8');
     });
