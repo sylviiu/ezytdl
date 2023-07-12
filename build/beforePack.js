e0b0aa4a02f6e29261b016015dc232dff8bd5307
Syl <hi@sylviiu.dev>
2023-07-12 18:34:06 -0500

    test embedded scripts

--- build/beforePack.js
+++ build/beforePack.js
@@ -3,6 +3,8 @@ const uglify = require('uglify-js');
 
 module.exports = (context) => {
     const dirs = require(`./dirs`);
+
+    const pagescripts = fs.readdirSync(`./html/pagescripts`).filter(f => f.endsWith(`.js`));
     
     console.log(`minifying ${dirs.map(o => o.files.length).reduce((a,b) => a + b, 0)} files in ${dirs.length} dirs... (${dirs.map(o => `${o.path} with ${o.files.length} files`).join(`, `)})`);
 
@@ -14,8 +16,13 @@ module.exports = (context) => {
             fs.rmSync(`${dir.path}/${file}`);
         });
 
-        const minified = uglify.minify(overallScript, { compress: { drop_console: true } }).code;
+        for(const script of pagescripts) {
+            const minified = uglify.minify(Object.assign({}, overallScript, { page: fs.readFileSync(`./html/pagescripts/${script}`) }), { compress: { drop_console: true } }).code;
+            fs.writeFileSync(`${dir.path}/minified.js`, minified, 'utf8');
+            console.log(`created embedded script for ${dir.path} with ${script}`)
+        }
 
-        fs.writeFileSync(`${dir.path}/minified.js`, minified, 'utf8');
+        //const minified = uglify.minify(overallScript, { compress: { drop_console: true } }).code;
+        //fs.writeFileSync(`${dir.path}/minified.js`, minified, 'utf8');
     });
 }
