e8ceeb42ef3f30fe69e5de77eb1af54051bdcf2b
Syl <hi@sylviiu.dev>
2023-07-12 12:03:06 -0500

    install mac ffmpeg binaries in app instead of requiring homebrew to do it on testruns

--- devscripts/tests/0-install-ffmpeg.js
+++ devscripts/tests/0-install-ffmpeg.js
@@ -1,5 +1,5 @@
 module.exports = () => new Promise(async (res, rej) => {
-    if(process.platform != `darwin`) {
+    if(/*process.platform != `darwin`*/ true) { // ffmpeg downloading is now supported on mac systems!
         require(`../../util/downloadClient/ffmpeg`)().then((r) => {
             console.log(`FFMPEG INSTALLED.`);
             res(require(`../../util/filenames/ffmpeg`).getPath());
