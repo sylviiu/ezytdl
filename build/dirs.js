eb4ec2a1e4c0a487ccd17ad539c287fb76936bf0
Syl <hi@sylviiu.dev>
2023-07-12 18:08:52 -0500

    ignore pagescripts

--- build/dirs.js
+++ build/dirs.js
@@ -1,6 +1,6 @@
 const fs = require('fs');
 
-const blacklistedDirs = [`assets`];
+const blacklistedDirs = [`assets`, `pagescripts`];
 
 const dirs = fs.readdirSync(`./html`).filter(f => !f.endsWith(`.html`) && blacklistedDirs.indexOf(f) === -1).map(f => ({ path: `./html/${f}`, files: fs.readdirSync(`./html/${f}`).filter(f2 => f2.endsWith(`.js`)) }));
 
