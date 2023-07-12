656fe56d11af5d95c490e9dbdf53fb4a7b76cc6e
Syl <hi@sylviiu.dev>
2023-07-12 18:08:49 -0500

    remove minified.js

--- build/afterPack.js
+++ build/afterPack.js
@@ -7,14 +7,12 @@ module.exports = (context) => {
     console.log(`removing ${dirs.map(o => o.files.length).reduce((a,b) => a + b, 0)} minified files in ${dirs.length} dirs... (${dirs.map(o => `${o.path} with ${o.files.length} files`).join(`, `)})`);
 
     dirs.forEach(dir => {
-        dir.files.forEach(file => {
-            try {
-                fs.rmSync(`${dir.path}/${file}`);
-                console.log(`removed file ${dir.path}/${file}`);
-            } catch(e) {
-                console.log(`Failed removing file ${dir.path}/${file}: ${e}`);
-            }
-        });
+        try {
+            fs.rmSync(`${dir.path}/minified.js`);
+            console.log(`removed file ${dir.path}/minified.js`);
+        } catch(e) {
+            console.log(`Failed removing file ${dir.path}/minified.js: ${e}`);
+        }
     });
 
     console.log(`resetting repo...`);
