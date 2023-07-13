dfe0cf13e1cd1cb89f409b12b76ba8fea7e1baec
Syl <hi@sylviiu.dev>
2023-07-12 19:43:07 -0500

    ignore minified

--- build/dirs.js
+++ build/dirs.js
@@ -2,6 +2,6 @@ const fs = require('fs');
 
 const blacklistedDirs = [`assets`, `pagescripts`];
 
-const dirs = fs.readdirSync(`./html`).filter(f => !f.endsWith(`.html`) && blacklistedDirs.indexOf(f) === -1).map(f => ({ path: `./html/${f}`, files: fs.readdirSync(`./html/${f}`).filter(f2 => f2.endsWith(`.js`)) }));
+const dirs = fs.readdirSync(`./html`).filter(f => !f.endsWith(`.html`) && blacklistedDirs.indexOf(f) === -1).map(f => ({ path: `./html/${f}`, files: fs.readdirSync(`./html/${f}`).filter(f2 => f2 != `minified.js` && f2.endsWith(`.js`)) }));
 
 module.exports = dirs;
