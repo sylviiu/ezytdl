359c88dec2da6bb3a299558bac5669f43ae6e7c5
Syl <hi@sylviiu.dev>
2023-07-17 12:36:40 -0500

    fix updating

--- util/updateAvailable/ezytdl.js
+++ util/updateAvailable/ezytdl.js
@@ -2,5 +2,5 @@ module.exports = {
     name: `ezytdl`,
     //description: `The main application.`,
     description: `<img src=\"../res/img/steamhappy.webp\" height=\"20\"/>`,
-    check: () => require(`../../core/checkForUpdates`)()
+    check: () => require(`../../core/checkForUpdates`)(true)
 };
