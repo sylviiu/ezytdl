f78f8b261efafa8c06f11fca78e68aa1505fdc0c
Syl <hi@sylviiu.dev>
2023-07-21 23:53:16 -0500

    fix error exit

--- util/errorAndExit.js
+++ util/errorAndExit.js
@@ -2,13 +2,15 @@ const createDialog = require(`./createDialog`);
 
 const { app } = require('electron');
 
-module.exports = (msg) => {
+module.exports = async (msg) => {
+    if(!app.isReady() && !global.testrun) await app.whenReady();
+
     if(!app.isReady() || global.testrun) {
         console.error(`APP NOT READY, HERE'S AN ERROR LOG`, msg);
         process.exit(1);
     } else {
         createDialog(`error`, `Failed to finish startup process!`, typeof msg == `object` ? JSON.stringify(msg, null, 4) : msg);
         global.quitting = true;
-        //return app.quit()
+        require(`../core/quit`).quit();
     }
 }
