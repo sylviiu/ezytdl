988d3a6eea4539077660157fef1cc77f94d085fb
Syl <hi@sylviiu.dev>
2023-07-21 23:54:25 -0500

    use "run-script" launch param instead

--- index.js
+++ index.js
@@ -5,7 +5,9 @@ global.startTime = startTime;
 global.testrun = process.argv.find(s => s == `--testrun`) ? true : false;
 global.headless = process.argv.find(s => s == `--headless`) ? true : false;
 
-console.log(`Starting ezytdl v${require(`./package.json`).version}`)
+const mode = (require(`yargs`).parse(process.argv)["run-script"] || `main`).toLowerCase();
+
+console.log(`Starting ezytdl v${require(`./package.json`).version} in mode "${mode}"`)
 
 const { app, ipcMain } = require(`electron`);
 
@@ -19,8 +21,11 @@ global.configPath = app.getPath('userData');
 process.on(`uncaughtException`, (err) => (global.testrun ? require(`./util/errorAndExit`) : require(`./util/errorHandler`))(`${typeof err == `object` ? JSON.stringify(err, null, 4) : err}\n\n${err.stack? err.stack : `(no stack)`}`))
 process.on(`unhandledRejection`, (err) => (global.testrun ? require(`./util/errorAndExit`) : require(`./util/errorHandler`))(`${typeof err == `object` ? JSON.stringify(err, null, 4) : err}\n\n${err.stack? err.stack : `(no stack)`}`))
 
-if(process.argv.find(s => s == `--extension`)) {
+const fs = require(`fs`);
+const getPath = require(`./util/getPath`);
 
-} else require(`./system/main`)()
+if(getPath(`./system/${mode}.js`, true)) {
+    require(`./system/${mode}.js`)();
+} else return require(`./util/errorHandler`)(`Mode "${mode}" not found!`)
 
 process.on(`SIGINT`, require(`./core/quit`).quit);
