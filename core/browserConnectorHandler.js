79c783d4010b4dce138af994b864104fa11ff9cb
Syl <hi@sylviiu.dev>
2023-07-28 06:04:13 -0500

    log data (temp)

--- core/browserConnectorHandler.js
+++ core/browserConnectorHandler.js
@@ -2,6 +2,6 @@ module.exports = ({ session, id }) => {
     console.log(`[extension/${id}] creating message handler...`);
 
     return ({ type, data }) => {
-        console.log(`[extension/${id} AUTHED] received message! (type: ${type})`);
+        console.log(`[extension/${id} AUTHED] received message! (type: ${type})`, data);
     }
 }
