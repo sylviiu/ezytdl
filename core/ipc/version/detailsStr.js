b9248a0e80c05f774667e58d9777aec27d5fc55d
Syl <hi@sylviiu.dev>
2023-07-30 18:28:48 -0500

    move detailsStr from "system" to "version" ipc

--- core/ipc/version/detailsStr.js
+++ core/ipc/version/detailsStr.js
@@ -1,30 +1,32 @@
+const getPath = require(`../../../util/getPath`);
+
 const month = [`Jan`, `Feb`, `Mar`, `Apr`, `May`, `Jun`, `Jul`, `Aug`, `Sep`, `Oct`, `Nov`, `Dec`]
 const dayOfWeek = [`Sunday`, `Monday`, `Tuesday`, `Wednesday`, `Thursday`, `Friday`, `Saturday`]
-
-const pkg = require(`../../../package.json`);
-
-console.log(pkg)
-
-const appVersions = {
-    "ezytdl": {
-        "Version": pkg.version,
-        "Commit Hash": pkg.fullCommitHash || `unknown`,
-        "Built": pkg.buildDate || global.startTime,
-    },
-    "Node.JS": {
-        "Version": `${process.versions.node} (${process.release.lts || process.release.name})`,
-        "V8": process.versions.v8,
-        "Platform": process.platform,
-        "Arch": process.arch,
-    },
-    "Electron": {
-        "Version": process.versions.electron,
-    }
-}
 
 module.exports = {
     type: `handle`,
     func: (_e, d) => new Promise(async res => {
+        const package = require(`../../../package.json`);
+
+        let appVersions = {};
+
+        if(package.buildInfo) {
+            appVersions = package.buildInfo
+        } else if(await getPath(`./build.js`, true, false, true)) {
+            appVersions = (await require(`../../../build.js`).getFullMetadata()).extraMetadata.buildInfo
+        } else if(await getPath(`./build.json`, true, false, true)) {
+            appVersions = require(`../../../build.json`).extraMetadata.buildInfo
+        }
+        
+        Object.assign(appVersions, {
+            "Node.JS": {
+                "Version": `${process.versions.node} (${process.release.lts || process.release.name})`,
+                "V8": process.versions.v8,
+                "Platform": process.platform,
+                "Arch": process.arch,
+            },
+        });
+
         const details = [];
 
         await require(`../../../util/currentVersion/pybridge`)()
@@ -33,7 +35,7 @@ module.exports = {
 
         console.log(useObj)
 
-        Object.keys(useObj).forEach(pkg => {
+        Object.keys(useObj).filter(s => !s.includes(`Libraries`)).forEach(pkg => {
             details.push(``, `#### ${pkg}`);
             Object.entries(useObj[pkg]).forEach(([k, v]) => {
                 if(k.toLowerCase() == `built`) {
@@ -51,6 +53,13 @@ module.exports = {
                 };
                 
                 details.push(`- ${k}: ${v}`)
+            });
+        });
+
+        Object.keys(useObj).filter(s => s.includes(`Libraries`)).forEach(pkg => {
+            details.push(``, `#### ${pkg}`);
+            Object.entries(useObj.Libraries).forEach(([k, v]) => {
+                details.push(`- [${k}](https://www.npmjs.com/package/${k}): [${v}](https://www.npmjs.com/package/${k}/v/${v})`)
             });
         });
 
