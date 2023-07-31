1b86ae1fba1e3e199d38644cbcc9ebcf799d70cf
Syl <hi@sylviiu.dev>
2023-07-31 13:41:01 -0500

    fix to #36 -- don't run depchecks if ldconfig is not found

--- core/depcheck/linux-library.js
+++ core/depcheck/linux-library.js
@@ -1,11 +1,15 @@
-global.ldconfig = require(`child_process`).execFileSync(`ldconfig`, [`-p`]).toString().split(`\n\t`).map(s => s.split(` `)[0]);
-
 module.exports = (name) => {
-    const included = global.ldconfig && global.ldconfig.find(s => s == module.exports.name)
-    console.log(`checking for ${name} | ${included}`)
-    if(included) {
-        return true
-    } else {
-        return false
+    try {
+        global.ldconfig = require(`child_process`).execFileSync(`ldconfig`, [`-p`]).toString().split(`\n\t`).map(s => s.split(` `)[0]);
+        
+        const included = global.ldconfig && global.ldconfig.find(s => s == module.exports.name)
+        console.log(`checking for ${name} | ${included}`)
+        if(included) {
+            return true
+        } else {
+            return false
+        }
+    } catch(e) {
+        console.log(`ldconfig not found, continuing`)
     }
 }
