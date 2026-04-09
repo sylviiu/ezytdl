3e12deb1e12a31a4fd9436901faaef1d1794ff52
Syl <hi@sylviiu.dev>
2026-04-08 19:32:56 -0500

    hex fallback

--- util/hexToRGB.js
+++ util/hexToRGB.js
@@ -3,6 +3,8 @@ module.exports = (hex) => {
 
     if(hex.length === 3) hex = hex.split(``).map(c => c.repeat(2)).join(``);
 
+    if(!hex) hex = "b981fe";
+    
     const [r, g, b] = hex.match(/.{2}/g).map(c => parseInt(c, 16));
 
     return { r, g, b };
